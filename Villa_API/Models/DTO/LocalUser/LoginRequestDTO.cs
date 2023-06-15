bf4b18c3565fcbb5c4e383c36aef1d0b1fbaf264
Alvi Hasan <livealvi@gmail.com>
2023-06-15 14:45:42 +0600

    add: user dto and repository

---
+++ Villa_API/Models/DTO/LocalUser/LoginRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Villa_API.Models.DTO.User
+{
+    public class LoginRequestDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
+
---
+++ Villa_API/Models/DTO/LocalUser/LoginResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Villa_API.Models.DTO.User
+{
+	public class LoginResponseDTO
+	{
+		public LoginResponseDTO()
+		{
+		}
+	}
+}
+
